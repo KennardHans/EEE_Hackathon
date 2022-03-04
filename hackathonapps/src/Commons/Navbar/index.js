1b13a3efb333ffe04e869d36ce5d24bb7eb2539b
KennardHans <kennard112356@gmail.com>
2022-03-04 18:38:31 +0800

    Added Header Wireframe

---
+++ hackathonapps/src/Commons/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './style.css'
+
+export const Navbar = () => {
+    return ( 
+        <div className="Navbar">
+            <div className="Navbar__left">
+                <div className="Navbar__left__img">Image</div>
+                <div className="Navbar__left__title">TITLE</div>
+            </div>
+            <div className="Navbar__right">
+                <div className="Navbar__right__content">FAQs</div>
+                <div className="Navbar__right__content">About</div>
+                <div className="Navbar__right__content">Log In</div>
+                <div className="Navbar__right__content">Sign Up</div>
+            </div>
+        </div>
+     );
+}
+ 
---
+++ hackathonapps/src/routes.js
@@ -0,0 +1,14 @@
+export const routes = [
+    {
+        id:1,
+        name:"Home",
+        component:<Home/>,
+        path:"/",
+    },
+    {
+        id:2,
+        name:"Next",
+        component:<Next/>,
+        path:"/next",
+    }
+]
